5d59e24997770ccaf6001a2429b22978ca397a5f
milon2267 <gdazad2267@gmail.com>
2022-07-18 13:01:21 +0600

    Routing setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+import ToDoApp from "./components/ToDoApp";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/signin" element={<Signin/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/todoapp" element={<ToDoApp/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ToDoApp from "./ToDoApp";
+
+const Home = () => {
+  return (
+    <>
+        <ToDoApp></ToDoApp>
+    </>
+  )
+}
+
+export default Home
