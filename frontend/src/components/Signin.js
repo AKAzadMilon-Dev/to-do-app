b665195c2e9c85301a471bd22e689b128a26f587
milon2267 <gdazad2267@gmail.com>
2022-07-18 17:49:55 +0600

    signin and signup design

--- frontend/src/components/Signin.js
+++ frontend/src/components/Signin.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import {Col, Container, Row, Form, Button, Alert} from 'react-bootstrap';
 
 const Signin = () => {
   return (
-    <div>Signin</div>
+    <Container className='w-25 border mt-5'>
+        <Row >
+            <Col lg={12}>
+                <h3 className='headText'>Signin</h3>
+                <Form className='singinform'>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+
+                    <div className="d-grid gap-2">
+                        <Button variant="primary" size="md">Signin</Button>
+                    </div>
+                    <Alert className='alertStyle'>
+                        Dont have an account
+                    </Alert>
+                </Form>
+            </Col>
+        </Row>
+    </Container>
   )
 }
 
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React from 'react';
+import {Col, Container, Row, Form, Button, Alert} from 'react-bootstrap';
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <Container className='w-25 border mt-5'>
+        <Row >
+            <Col lg={12}>
+                <Form className='singinform'>
+                <h3 className='headText'>Signup</h3>
+                    <Form.Group className="mb-3" >
+                        <Form.Label>Full Name</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Full Name" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" >
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter Email" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" >
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" >
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control type="password" placeholder="Confirm Password" />
+                    </Form.Group>
+
+                    <div className="d-grid gap-2">
+                        <Button variant="primary" size="md">Signin</Button>
+                    </div>
+                    <Alert className='alertStyle'>
+                        Dont have an account
+                    </Alert>
+                </Form>
+            </Col>
+            <Col lg={6}>
+            
+            </Col>
+        </Row>
+    </Container>
   )
 }
 
